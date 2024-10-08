a45ddd2131d8ec30de13462d8cf18d61c1a7adb8
nikhilhore <horenikhil5@gmail.com>
2024-10-08 20:16:43 +0530

    Astronomy screen torch final version

--- AstronomyScreenTorch/ViewController.swift
+++ AstronomyScreenTorch/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var toggleTorchButton: UIButton = {
+        let button = UIButton()
+        button.configuration = .filled()
+        button.setTitle("Toggle Torch", for: .normal)
+        button.addTarget(
+            self,
+            action: #selector(toggleTorch),
+            for: .touchUpInside
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
 
+    func setup() {
+        view.addSubview(toggleTorchButton)
+        NSLayoutConstraint.activate([
+            toggleTorchButton.centerXAnchor
+                .constraint(equalTo: view.centerXAnchor),
+            toggleTorchButton.centerYAnchor
+                .constraint(equalTo: view.centerYAnchor),
+        ])
+    }
 
+    @objc func toggleTorch() {
+        if view.backgroundColor == .blue {
+            view.backgroundColor = .red
+        } else {
+            view.backgroundColor = .blue
+        }
+    }
 }
-
